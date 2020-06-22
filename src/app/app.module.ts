8acdd9d83e7830c76159a4b0c7dd0e8973c261c4
quiyoshi <cs15035@s.inf.shizuoka.ac.jp>
2020-06-22 20:50:20 +0900

    9 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,34 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    MatCardModule,
+    MatCheckboxModule,
+  ],
   declarations: [ AppComponent, HelloComponent, TodoListComponent, TodoFormComponent ],
   bootstrap:    [ AppComponent ]
 })
